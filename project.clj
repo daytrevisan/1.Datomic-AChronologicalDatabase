8492b6e9eb7dc409b49fdc31c66039e29a517fd0
Dayane Trevisan <dayane.trevisan@nubank.com.br>
2025-02-26 20:05:51 -0300

    including missing settings

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject ecommerce "0.1.0-SNAPSHOT"
             :description "Ecommerce with Datomic"
             :dependencies [[org.clojure/clojure "1.11.1"]
-                           [com.datomic/datomic-pro "1.0.7277"]]
+                           [com.datomic/datomic-pro "1.0.6711"]
+                           [ch.qos.logback/logback-classic "1.2.3"]]
             :source-paths ["src"]
             :resource-paths ["resources"]
             :test-paths ["test"]
